9ab5ce72489d756e46319d36a6358eaacdbda0a8
Documenter.jl <documenter@juliadocs.github.io>
2025-11-07 12:42:53 +0000

    build based on fee74b6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DiscreteNaturalNeighbors","page":"Home","title":"DiscreteNaturalNeighbors","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DiscreteNaturalNeighbors.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
