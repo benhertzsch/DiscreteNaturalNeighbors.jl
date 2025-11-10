4f333223716d35fa164c80c421c9a0fc0f732f8a
Benjamin Hertzsch <benjamin.hertzsch@ed.ac.uk>
2025-11-10 15:42:46 +0000

    Add codecov and first runtests.jl version

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,21 @@ using DiscreteNaturalNeighbors
 using Test
 
 @testset "DiscreteNaturalNeighbors.jl" begin
-    # Write your tests here.
+    
+    using JLD2
+
+    # --- test 2D interpolation routines --- #
+    dict = load("data/data_2D.jld2")
+    points, values = dict["points"], dict["values"]
+
+    xRange, yRange = -5:0.05:5, -5:0.05:5
+    interp_a = interpolate2D(points, values, (xRange, yRange));
+    interp_b = interpolateND(points, values, (xRange, yRange));
+
+    @test size(interp_a) == (201, 201)
+    @test size(interp_b) == (201, 201)
+    @test interp_a[107, 48] ≈ 0.5589506815196893
+    @test all(interp_a .≈ interp_b)
+
+
 end
